5e33fe0f5f17ff81e18e8897e271c4e78bdda988
Sandra <sandra.mar05@iest.edu.mx>
2025-09-29 18:04:52 -0600

    Initial Commit

---
+++ Navegacion/NavegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionApp.swift
+//  Navegacion
+//
+//  Created by WIN603 on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NavegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
